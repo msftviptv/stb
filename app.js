b9cbcb399c5517e253ec99bc2956cb84736cf657
msftviptv <64074648+msftviptv@users.noreply.github.com>
2020-04-21 22:33:39 +0200

    mac

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const form = document.querySelector('#add-kobraiptv-form');
 
 function rendermac(doc){
     let li = document.createElement('li');
-    let kobraiptv = document.createElement('span');
+    let mac = document.createElement('span');
     let cross = document.createElement('div');
 
         li.setAttribute('data-id', doc.id);
         mac.textContent = doc.data().mac;
         cross.textContent = 'x';
 
-        li.appendChild(kobraiptv);
+        li.appendChild(mac);
         li.appendChild(cross);
 
         macList.appendChild(li);
@@ -18,13 +18,13 @@ function rendermac(doc){
  cross.addEventListener('click', (e) => {
     e.stopPropagation();
     let id = e.target.parentElement.getAttribute('data-id');
-    db.collection('kobraiptv').doc(id).delete();
+    db.collection('mac').doc(id).delete();
 });
 }
 
 
 //getting data
-db.collection('kobraiptv').get().then((snapshot) => {
+db.collection('mac').get().then((snapshot) => {
     snapshot.docs.forEach(doc =>{
         rendermac(doc);
     })
@@ -34,10 +34,10 @@ db.collection('kobraiptv').get().then((snapshot) => {
 //saving data
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
-    db.collection('kobraiptv').add({
-        kobraiptv :form.kobraiptv.value
+    db.collection('mac').add({
+        mac :form.mac.value
     });
-    form.kobraiptv.value ='';
+    form.mac.value ='';
 });
 
  
