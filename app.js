4228514a0f41a9c6e8118250c6770cfb6a21669c
msftviptv <64074648+msftviptv@users.noreply.github.com>
2020-04-21 16:01:10 +0200

    v1
    يارب يشتغل

---
+++ app.js
@@ -0,0 +1,45 @@
+const macList = document.querySelector('#mac-list');
+const form = document.querySelector('#add-mac-form');
+
+function rendermac(doc){
+    let li = document.createElement('li');
+    let mac = document.createElement('span');
+    let cross = document.createElement('div');
+
+        li.setAttribute('data-id', doc.id);
+        mac.textContent = doc.data().mac;
+        cross.textContent = 'x';
+
+        li.appendChild(mac);
+        li.appendChild(cross);
+
+        macList.appendChild(li);
+         // deleting data
+ cross.addEventListener('click', (e) => {
+    e.stopPropagation();
+    let id = e.target.parentElement.getAttribute('data-id');
+    db.collection('stb').doc(id).delete();
+});
+}
+
+
+//getting data
+db.collection('stb').get().then((snapshot) => {
+    snapshot.docs.forEach(doc =>{
+        rendermac(doc);
+    })
+});
+
+
+//saving data
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    db.collection('stb').add({
+        mac :form.mac.value
+    });
+    form.mac.value ='';
+});
+
+ 
+
+ 
